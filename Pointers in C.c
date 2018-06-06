10d37d215e86d694c26a794687e2122d5a1e71d2
Akash Macha <akashmacha1999@gmail.com>
2018-06-06 14:09:23 +0530

    Create Pointers in C.c

---
+++ Pointers in C.c
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/pointer-in-c/problem
+#include <stdio.h>
+#include <math.h>
+void update(int *a,int *b) {
+    int tempA = *a;
+    *a = *a + *b;
+    *b = abs(tempA - *b);
+    return;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
